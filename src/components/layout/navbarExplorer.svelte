57fd909bea7e2d56b697e1e08833c445ab71cbcf
Meibbo <meibbomail@gmail.com>
2026-04-18 05:54:38 -0500

    addition commit

--- src/components/layout/navbarExplorer.svelte
+++ src/components/layout/navbarExplorer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { translate } from "../../i18n/index";
-  import SortPopup from "./popupSort.svelte";
-  import ViewModePopup from "./popupView.svelte";
+  import SortPopup from "./menuSort.svelte";
+  import ViewModePopup from "./menuView.svelte";
   import { explorerFiles } from "../containers/explorerFiles";
   import { explorerProps } from "../containers/explorerProps";
   import { explorerTags } from "../containers/explorerTags";
@@ -17,9 +17,6 @@
     sortDirection = $bindable("asc"),
     viewMode = $bindable("tree"),
     addMode = $bindable(false),
-    tagsExplorer,
-    propExplorer,
-    fileList,
     icon,
     addOpCount = 0,
   }: {
@@ -43,22 +40,40 @@
     files: ["lucide-file", "lucide-folder"],
   };
   const CATEGORY_LABELS: Record<FiltersTab, [string, string]> = {
-    props: [translate("filter.category.props"), translate("filter.category.values")],
-    tags:  [translate("filter.category.all_tags"), translate("filter.category.leaf_tags")],
-    files: [translate("filter.category.files"), translate("filter.category.folders")],
+    props: [
+      translate("filter.category.props"),
+      translate("filter.category.values"),
+    ],
+    tags: [
+      translate("filter.category.all_tags"),
+      translate("filter.category.leaf_tags"),
+    ],
+    files: [
+      translate("filter.category.files"),
+      translate("filter.category.folders"),
+    ],
   };
 
   const currentCategoryIcon = $derived(
-    CATEGORY_ICONS[activeTab]?.[filtersSearchCategory[activeTab] ?? 0] ?? "lucide-search",
+    CATEGORY_ICONS[activeTab]?.[filtersSearchCategory[activeTab] ?? 0] ??
+      "lucide-search",
   );
 
-  let headerMode      = $state<HeaderMode>("header");
-  let headerExitDir   = $state<"left" | "right">("right");
+  let headerMode = $state<HeaderMode>("header");
+  let headerExitDir = $state<"left" | "right">("right");
   let currentViewMode = $state<"tree" | "dnd" | "grid" | "cards">("grid"); // Files default is grid
 
-  function openSortPopup()     { headerExitDir = "right"; headerMode = "sort";     }
-  function openViewModePopup() { headerExitDir = "left";  headerMode = "viewmode"; }
-  function closeHeaderPopup()  { headerMode = "header"; }
+  function openSortPopup() {
+    headerExitDir = "right";
+    headerMode = "sort";
+  }
+  function openViewModePopup() {
+    headerExitDir = "left";
+    headerMode = "viewmode";
+  }
+  function closeHeaderPopup() {
+    headerMode = "header";
+  }
 
   function cycleSearchCategory() {
     const tab = activeTab;
@@ -72,71 +87,86 @@
     {#if headerMode === "header"}
       <div class="vaultman-filters-header">
         <div
-          class="vaultman-nav-fab"
+          class="vaultman-nav-icon is-active"
           role="button"
           tabindex="0"
           aria-label={translate("filter.viewmode_btn")}
           onclick={openViewModePopup}
-          onkeydown={(e: KeyboardEvent) => { if (e.key === 'Enter' || e.key === ' ') openViewModePopup(); }}
+          onkeydown={(e: KeyboardEvent) => {
+            if (e.key === "Enter" || e.key === " ") openViewModePopup();
+          }}
           use:icon={"lucide-layout-list"}
         ></div>
         <div class="vaultman-filters-header-search-pill">
           <input
             class="vaultman-filters-search-input"
-            type="text" autocomplete="off" autocorrect="off"
-            autocapitalize="off" spellcheck="false"
+            type="text"
+            autocomplete="off"
+            autocorrect="off"
+            autocapitalize="off"
+            spellcheck="false"
             placeholder={translate("filter.search_placeholder")}
             bind:value={filtersSearch}
           />
           {#if filtersSearch}
-            <button class="vaultman-filters-search-clear"
+            <button
+              class="vaultman-filters-search-clear"
               aria-label={translate("filter.search_clear")}
               use:icon={"lucide-x"}
-              onclick={() => { filtersSearch = ""; }}
+              onclick={() => {
+                filtersSearch = "";
+              }}
             ></button>
           {/if}
-          <button class="vaultman-filters-search-mode"
-            aria-label={CATEGORY_LABELS[activeTab]?.[filtersSearchCategory[activeTab] ?? 0] ?? translate("filter.search_mode")}
+          <button
+            class="vaultman-filters-search-mode"
+            aria-label={CATEGORY_LABELS[activeTab]?.[
+              filtersSearchCategory[activeTab] ?? 0
+            ] ?? translate("filter.search_mode")}
             use:icon={currentCategoryIcon}
             onclick={cycleSearchCategory}
           ></button>
         </div>
         <div
-          class="vaultman-nav-fab"
+          class="vaultman-nav-icon is-active"
           role="button"
           tabindex="0"
           aria-label={translate("filter.sort_btn")}
           onclick={openSortPopup}
-          onkeydown={(e: KeyboardEvent) => { if (e.key === 'Enter' || e.key === ' ') openSortPopup(); }}
+          onkeydown={(e: KeyboardEvent) => {
+            if (e.key === "Enter" || e.key === " ") openSortPopup();
+          }}
           use:icon={"lucide-arrow-up-down"}
         ></div>
       </div>
     {:else if headerMode === "sort"}
-      <div class="vaultman-filters-popup-slot"
+      <div
+        class="vaultman-filters-popup-slot"
         class:popup-enter-from-left={headerExitDir === "right"}
         class:popup-enter-from-right={headerExitDir === "left"}
       >
-        <SortPopup 
-          {activeTab} 
+        <SortPopup
+          {activeTab}
           onClose={closeHeaderPopup}
           bind:sortBy
           bind:sortDir={sortDirection}
-          {icon} 
+          {icon}
         />
       </div>
     {:else if headerMode === "viewmode"}
-      <div class="vaultman-filters-popup-slot"
+      <div
+        class="vaultman-filters-popup-slot"
         class:popup-enter-from-left={headerExitDir === "right"}
         class:popup-enter-from-right={headerExitDir === "left"}
       >
-        <ViewModePopup 
-          {activeTab} 
+        <ViewModePopup
+          {activeTab}
           onClose={closeHeaderPopup}
           bind:viewMode
           bind:addMode
           initialViewMode={viewMode}
           {addOpCount}
-          {icon} 
+          {icon}
         />
       </div>
     {/if}
--- src/components/pages/pageFilters.svelte
+++ src/components/pages/pageFilters.svelte
@@ -1,142 +1,151 @@
 <script lang="ts">
-	import type { VaultmanPlugin } from "../../../main";
-	import { setIcon } from "obsidian";
-	import FiltersTagsTab from "./tabTags.svelte";
-	import FiltersPropsTab from "./tabProps.svelte";
-	import FiltersFilesTab from "./tabFiles.svelte";
-	import NavbarTabs from "../layout/navbarTabs.svelte";
-	import NavbarFilters from "../layout/navbarFilters.svelte";
-	import { explorerFiles } from "../containers/explorerFiles";
-	import { explorerProps } from "../containers/explorerProps";
-	import { explorerTags } from "../containers/explorerTags";
+  import type { VaultmanPlugin } from "../../../main";
+  import { setIcon } from "obsidian";
+  import FiltersTagsTab from "./tabTags.svelte";
+  import FiltersPropsTab from "./tabProps.svelte";
+  import FiltersFilesTab from "./tabFiles.svelte";
+  import NavbarPages from "../layout/navbarPages.svelte";
+  import NavbarExplorer from "../layout/navbarExplorer.svelte";
+  import { explorerFiles } from "../containers/explorerFiles";
+  import { explorerProps } from "../containers/explorerProps";
+  import { explorerTags } from "../containers/explorerTags";
 
-	type FiltersTab = "props" | "files" | "tags";
+  type FiltersTab = "props" | "files" | "tags";
 
-	let {
-		plugin,
-		filtersActiveTab = $bindable("props"),
-		filtersSearch = $bindable(""),
-		filtersSearchCategory = $bindable({ tags: 0, props: 0, files: 0 }),
-		filtersSortBy = $bindable("name"),
-		filtersSortDir = $bindable("asc"),
-		filtersViewMode = $bindable("tree"),
-		addMode = $bindable(false),
-		tagsExplorer = $bindable(),
-		propExplorer = $bindable(),
-		fileList = $bindable(),
-		selectedCount = $bindable(0),
-		selectedFilePaths = $bindable(new Set<string>()),
-		addOpCount = 0,
-	}: {
-		plugin: VaultmanPlugin;
-		filtersActiveTab: FiltersTab;
-		filtersSearch: string;
-		filtersSearchCategory: Record<FiltersTab, number>;
-		filtersSortBy?: string;
-		filtersSortDir?: "asc" | "desc";
-		filtersViewMode?: any;
-		addMode?: boolean;
-		tagsExplorer?: explorerTags | undefined;
-		propExplorer?: explorerProps | undefined;
-		fileList?: explorerFiles | undefined;
-		selectedCount?: number;
-		selectedFilePaths?: Set<string>;
-		addOpCount?: number;
-	} = $props();
+  let {
+    plugin,
+    filtersActiveTab = $bindable("props"),
+    filtersSearch = $bindable(""),
+    filtersSearchCategory = $bindable({ tags: 0, props: 0, files: 0 }),
+    filtersSortBy = $bindable("name"),
+    filtersSortDir = $bindable("asc"),
+    filtersViewMode = $bindable("tree"),
+    addMode = $bindable(false),
+    tagsExplorer = $bindable(),
+    propExplorer = $bindable(),
+    fileList = $bindable(),
+    selectedCount = $bindable(0),
+    selectedFilePaths = $bindable(new Set<string>()),
+    addOpCount = 0,
+  }: {
+    plugin: VaultmanPlugin;
+    filtersActiveTab: FiltersTab;
+    filtersSearch: string;
+    filtersSearchCategory: Record<FiltersTab, number>;
+    filtersSortBy?: string;
+    filtersSortDir?: "asc" | "desc";
+    filtersViewMode?: any;
+    addMode?: boolean;
+    tagsExplorer?: explorerTags | undefined;
+    propExplorer?: explorerProps | undefined;
+    fileList?: explorerFiles | undefined;
+    selectedCount?: number;
+    selectedFilePaths?: Set<string>;
+    addOpCount?: number;
+  } = $props();
 
-	function switchFiltersTab(tab: FiltersTab) {
-		if (filtersActiveTab === tab) return;
-		filtersActiveTab = tab;
-	}
+  function switchFiltersTab(tab: FiltersTab) {
+    if (filtersActiveTab === tab) return;
+    filtersActiveTab = tab;
+  }
 
-	function icon(el: HTMLElement, name: string) {
-		setIcon(el, name);
-		return {
-			update(n: string) {
-				setIcon(el, n);
-			},
-		};
-	}
+  function icon(el: HTMLElement, name: string) {
+    setIcon(el, name);
+    return {
+      update(n: string) {
+        setIcon(el, n);
+      },
+    };
+  }
 </script>
 
-<NavbarTabs
-	activeTab={filtersActiveTab}
-	showLabels={plugin.settings.filtersShowTabLabels}
-	onTabChange={switchFiltersTab}
-	{icon}
+<NavbarPages
+  activeTab={filtersActiveTab}
+  showLabels={plugin.settings.filtersShowTabLabels}
+  onTabChange={switchFiltersTab}
+  {icon}
 />
 
-<NavbarFilters
-	activeTab={filtersActiveTab}
-	bind:filtersSearch
-	bind:filtersSearchCategory
-	bind:sortBy={filtersSortBy}
-	bind:sortDirection={filtersSortDir}
-	bind:viewMode={filtersViewMode}
-	bind:addMode
-	{tagsExplorer}
-	{propExplorer}
-	{fileList}
-	{addOpCount}
-	{icon}
+<NavbarExplorer
+  activeTab={filtersActiveTab}
+  bind:filtersSearch
+  bind:filtersSearchCategory
+  bind:sortBy={filtersSortBy}
+  bind:sortDirection={filtersSortDir}
+  bind:viewMode={filtersViewMode}
+  bind:addMode
+  {tagsExplorer}
+  {propExplorer}
+  {fileList}
+  {addOpCount}
+  {icon}
 />
 
 <div class="vaultman-filters-tabs-container">
-	<div class="vaultman-tab-wrapper" class:is-hidden={filtersActiveTab !== "tags"}>
-		<FiltersTagsTab
-			{plugin}
-			bind:searchTerm={filtersSearch}
-			searchMode={filtersSearchCategory.tags}
-			bind:sortBy={filtersSortBy}
-			bind:sortDirection={filtersSortDir}
-			bind:viewMode={filtersViewMode}
-			bind:explorer={tagsExplorer}
-		/>
-	</div>
-	<div class="vaultman-tab-wrapper" class:is-hidden={filtersActiveTab !== "props"}>
-		<FiltersPropsTab
-			{plugin}
-			bind:searchTerm={filtersSearch}
-			searchMode={filtersSearchCategory.props}
-			bind:sortBy={filtersSortBy}
-			bind:sortDirection={filtersSortDir}
-			bind:viewMode={filtersViewMode}
-			bind:explorer={propExplorer}
-		/>
-	</div>
-	<div class="vaultman-tab-wrapper" class:is-hidden={filtersActiveTab !== "files"}>
-		<FiltersFilesTab
-			{plugin}
-			bind:searchTerm={filtersSearch}
-			searchMode={filtersSearchCategory.files}
-			bind:sortBy={filtersSortBy}
-			bind:sortDirection={filtersSortDir}
-			bind:viewMode={filtersViewMode}
-			bind:fileList
-			bind:selectedFilePaths
-			onSelectionChange={(c) => (selectedCount = c)}
-		/>
-	</div>
+  <div
+    class="vaultman-tab-wrapper"
+    class:is-hidden={filtersActiveTab !== "tags"}
+  >
+    <FiltersTagsTab
+      {plugin}
+      bind:searchTerm={filtersSearch}
+      searchMode={filtersSearchCategory.tags}
+      bind:sortBy={filtersSortBy}
+      bind:sortDirection={filtersSortDir}
+      bind:viewMode={filtersViewMode}
+      bind:explorer={tagsExplorer}
+    />
+  </div>
+  <div
+    class="vaultman-tab-wrapper"
+    class:is-hidden={filtersActiveTab !== "props"}
+  >
+    <FiltersPropsTab
+      {plugin}
+      bind:searchTerm={filtersSearch}
+      searchMode={filtersSearchCategory.props}
+      bind:sortBy={filtersSortBy}
+      bind:sortDirection={filtersSortDir}
+      bind:viewMode={filtersViewMode}
+      bind:explorer={propExplorer}
+    />
+  </div>
+  <div
+    class="vaultman-tab-wrapper"
+    class:is-hidden={filtersActiveTab !== "files"}
+  >
+    <FiltersFilesTab
+      {plugin}
+      bind:searchTerm={filtersSearch}
+      searchMode={filtersSearchCategory.files}
+      bind:sortBy={filtersSortBy}
+      bind:sortDirection={filtersSortDir}
+      bind:viewMode={filtersViewMode}
+      bind:fileList
+      bind:selectedFilePaths
+      onSelectionChange={(c) => (selectedCount = c)}
+    />
+  </div>
 </div>
 
 <style>
-	.vaultman-filters-tabs-container {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		overflow: hidden;
-		position: relative;
-	}
-	.vaultman-tab-wrapper {
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-		width: 100%;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-	.is-hidden {
-		display: none !important;
-	}
+  .vaultman-filters-tabs-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    position: relative;
+  }
+  .vaultman-tab-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .is-hidden {
+    display: none !important;
+  }
 </style>
