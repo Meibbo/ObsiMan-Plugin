ddd15890664746f7298cc7daaecf3054d768bbf7
Meibbo <meibbomail@gmail.com>
2026-05-01 15:31:28 -0500

    feat(primitives): add Toggle (Sub-A.3)

---
+++ src/components/primitives/Toggle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let {
+    checked = $bindable(false),
+    label,
+    disabled = false,
+    onChange,
+  }: {
+    checked: boolean;
+    label: string;
+    disabled?: boolean;
+    onChange?: (next: boolean) => void;
+  } = $props();
+</script>
+
+<label class="vm-toggle" class:is-disabled={disabled}>
+  <input type="checkbox" bind:checked={checked} {disabled} onchange={() => onChange?.(checked)} />
+  <span class="vm-toggle-knob"></span>
+  <span class="vm-toggle-label">{label}</span>
+</label>
