fb28c8c95dcf4cee89fcfb59f9dc304eaed8c956
Meibbo <meibbomail@gmail.com>
2026-05-01 15:32:26 -0500

    feat(primitives): add Dropdown (Sub-A.3)

---
+++ src/components/primitives/Dropdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" generics="T">
+  let {
+    value = $bindable<T>(),
+    options,
+    label,
+    disabled = false,
+  }: {
+    value: T;
+    options: { value: T; label: string }[];
+    label?: string;
+    disabled?: boolean;
+  } = $props();
+</script>
+
+<label class="vm-dropdown">
+  {#if label}<span class="vm-dropdown-label">{label}</span>{/if}
+  <select bind:value={value} {disabled}>
+    {#each options as opt (String(opt.value))}
+      <option value={opt.value}>{opt.label}</option>
+    {/each}
+  </select>
+</label>
